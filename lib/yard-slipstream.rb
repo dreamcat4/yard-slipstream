6930129d3b13487144028141d570fd3515fa63f2
Dreamcat4 <dreamcat4@gmail.com>
2010-04-10 08:16:42 +0100

    Remove old cruft

--- lib/yard-slipstream.rb
+++ lib/yard-slipstream.rb
@@ -3,16 +3,3 @@ $LOAD_PATH.unshift dir unless $LOAD_PATH.include?(dir)
 
 YARD::Templates::Engine.register_template_path dir + '/../templates'
 
-# require 'yard-slipstream/handler' if RUBY19
-# require 'yard-slipstream/legacy'
-
-# todo
-
-# * info box in top right
-# * search form in top right
-
-# rm -rf .yardoc doc ; /Users/id/tmp/yard/bin/yardoc -t "default" -p "/Users/id/tmp/yard/templates"
-
-# rm -rf .yardoc doc ; /Users/id/tmp/yard/bin/yardoc -t "default" -p "/Users/id/tmp/yard/templates" -e "/Users/id/tmp/yard-slipstream/lib/yard-slipstream.rb" lib/yard/templates/template.rb
-
-
