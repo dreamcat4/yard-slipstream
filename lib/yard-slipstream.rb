5fcd99e4787733bdd40e515da4ffbbcfda757cb6
Dreamcat4 <dreamcat4@gmail.com>
2010-04-11 12:01:15 +0100

    Break out of frames for external links. Stay inside frames for internal links.

--- lib/yard-slipstream.rb
+++ lib/yard-slipstream.rb
@@ -1,5 +1,26 @@
 dir = File.dirname(__FILE__)
 $LOAD_PATH.unshift dir unless $LOAD_PATH.include?(dir)
 
+require 'rubygems'
+require 'yard/core_ext/symbol_hash'
+require 'yard/templates/helpers/html_helper'
+
+module YARD
+  module Templates
+    module Helpers
+      module HtmlHelper
+        def link_url(url, title = nil, params = {})
+          params = SymbolHash.new(false).update(
+            :href => url,
+            :title  => h(title || url),
+            :target => "_self"
+          ).update(params)
+          "<a #{tag_attrs(params)}>#{title}</a>"
+        end
+      end
+    end
+  end
+end
+
 YARD::Templates::Engine.register_template_path dir + '/../templates'
 
